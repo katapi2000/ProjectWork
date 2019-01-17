13c552881fb608a62220bc6ff5afc6232496817d
katapi20 <katapi2000@gmail.com>
2019-01-17 12:49:50 +0900

    インデント直した

--- line18.c
+++ line18.c
@@ -20,50 +20,49 @@ void main() {
 		PORTB = 0b00000000;
     }
 
-    while(PORTAbits.RA4 || !flg) {
+    while(PORTAbits.RA4 || !flg) 
 
         flg = PORTAbits.RA4;
 
-        while(1) {  //ループ
-			sensor = PORTA & 0b1111;	//白黒反転
+    while(1) {  //ループ
+		sensor = PORTA & 0b1111;	//白黒反転
 
-			switch(sensor) {
-				case 0b1111:
-				case 0b1001:	//RA1,RA2白(前進)
-					PORTBbits.RB1 = PORTBbits.RB2 = 1;	//LED on
-					PORTBbits.RB4 = 1;	//左
-					PORTBbits.RB5 = 0;
-					PORTBbits.RB6 = 0;
-    				PORTBbits.RB7 = 1;	//右
-	    			__delay_ms(4);
-		    		PORTBbits.RB4 = 0;	//左
-    				PORTBbits.RB7 = 0;	//右
-	    			__delay_ms(5);
-					PORTBbits.RB1 = PORTBbits.RB2 = 0;	//LED off
-					break;
+		switch(sensor) {
+			case 0b1111:
+			case 0b1001:	//RA1,RA2白(前進)
+				PORTBbits.RB1 = PORTBbits.RB2 = 1;	//LED on
+				PORTBbits.RB4 = 1;	//左
+				PORTBbits.RB5 = 0;
+				PORTBbits.RB6 = 0;
+    			PORTBbits.RB7 = 1;	//右
+				__delay_ms(4);
+	    		PORTBbits.RB4 = 0;	//左
+    			PORTBbits.RB7 = 0;	//右
+				__delay_ms(5);
+				PORTBbits.RB1 = PORTBbits.RB2 = 0;	//LED off
+				break;
 
-				case 0b1011:	//RA2白
-				case 0b0111:	//RA3白
-					//右モータ回転
-					PORTBbits.RB2 = PORTBbits.RB3 = 1;	//LED on
-					PORTBbits.RB7 = 1;	//右
-		      		__delay_ms(4);
-			    	PORTBbits.RB7 = 0;	//右
-    				__delay_ms(5);
-					PORTBbits.RB2 = PORTBbits.RB3 = 0;	//LED off
-					break;
+			case 0b1011:	//RA2白
+			case 0b0111:	//RA3白
+				//右モータ回転
+				PORTBbits.RB2 = PORTBbits.RB3 = 1;	//LED on
+				PORTBbits.RB7 = 1;	//右
+	      		__delay_ms(4);
+		    	PORTBbits.RB7 = 0;	//右
+    			__delay_ms(5);
+				PORTBbits.RB2 = PORTBbits.RB3 = 0;	//LED off
+				break;
 
-				case 0b1110:	//RA0白
-				case 0b1101:	//RA1白
-					//左モータ回転
-					PORTBbits.RB0 = PORTBbits.RB1 = 1;	//LED on
-					PORTBbits.RB4 = 1;	//左
-	    			__delay_ms(4);
-		    		PORTBbits.RB4 = 0;	//左
-			    	__delay_ms(5);
-					PORTBbits.RB0 = PORTBbits.RB1 = 0;	//LED off
-					break;
-			}
-        }
+			case 0b1110:	//RA0白
+			case 0b1101:	//RA1白
+				//左モータ回転
+				PORTBbits.RB0 = PORTBbits.RB1 = 1;	//LED on
+				PORTBbits.RB4 = 1;	//左
+				__delay_ms(4);
+	    		PORTBbits.RB4 = 0;	//左
+		    	__delay_ms(5);
+				PORTBbits.RB0 = PORTBbits.RB1 = 0;	//LED off
+				break;
+		}
     }
 }
