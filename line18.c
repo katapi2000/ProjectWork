154528f777695bf00b94025a8bd1dbacd1b29aa6
katapi20 <katapi2000@gmail.com>
2019-01-17 12:51:55 +0900

    インデント直した

--- line18.c
+++ line18.c
@@ -25,40 +25,39 @@ void main() {
 		PORTB = 0b00000000;
     }
 
-    while(PORTAbits.RA4 || !flg) {
+    while(PORTAbits.RA4 || !flg) 
 		flg = PORTAbits.RA4;
 
-        while(1) {  //ループ
-			//sensor = ~PORTA;	//白黒反転
-			
-			if(PORTAbits.RA1 && PORTAbits.RA2) {	//前進
-				PORTBbits.RB1 = PORTBbits.RB2 = 1;	//LED on
-				PORTB = 0b10010000;
-		        __delay_ms(0.5);
-		        PORTB = 0b00000000;
-		        __delay_ms(0.4);
-				PORTBbits.RB1 = PORTBbits.RB2 = 0;	//LED off
-			}else if(!PORTAbits.RA0) {
-				PORTBbits.RB2 = PORTBbits.RB3 = 1;	//LED on
-				//左旋回(右モータ回転)
-				PORTB = 0b10000000;
-				__delay_ms(0.5);
+    while(1) {  //ループ
+		//sensor = ~PORTA;	//白黒反転
+		
+		if(PORTAbits.RA1 && PORTAbits.RA2) {	//前進
+			PORTBbits.RB1 = PORTBbits.RB2 = 1;	//LED on
+			PORTB = 0b10010000;
+	        __delay_ms(0.5);
+	        PORTB = 0b00000000;
+	        __delay_ms(0.4);
+			PORTBbits.RB1 = PORTBbits.RB2 = 0;	//LED off
+		}else if(!PORTAbits.RA0) {
+			PORTBbits.RB2 = PORTBbits.RB3 = 1;	//LED on
+			//左旋回(右モータ回転)
+			PORTB = 0b10000000;
+			__delay_ms(0.5);
 
-        		PORTB=0b00000000;
-       			 __delay_ms(0.4);
-       			course = LEFT;
-				PORTBbits.RB2 = PORTBbits.RB3 = 0;	//LED off
-			}else if(!PORTAbits.RA3) {
-				PORTBbits.RB0 = PORTBbits.RB1 = 1;	//LED on
-				//右旋回(左モータ回転)
-				PORTB = 0b00010000;
-				__delay_ms(0.5);
+    		PORTB=0b00000000;
+    		 __delay_ms(0.4);
+    		course = LEFT;
+			PORTBbits.RB2 = PORTBbits.RB3 = 0;	//LED off
+		}else if(!PORTAbits.RA3) {
+			PORTBbits.RB0 = PORTBbits.RB1 = 1;	//LED on
+			//右旋回(左モータ回転)
+			PORTB = 0b00010000;
+			__delay_ms(0.5);
 
-        		PORTB=0b00000000;
-       			__delay_ms(0.4);
-       			course = RIGHT;
-				PORTBbits.RB0 = PORTBbits.RB1 = 0;	//LED off
-			}
-        }
+    		PORTB=0b00000000;
+    		__delay_ms(0.4);
+    		course = RIGHT;
+			PORTBbits.RB0 = PORTBbits.RB1 = 0;	//LED off
+		}
     }
 }
